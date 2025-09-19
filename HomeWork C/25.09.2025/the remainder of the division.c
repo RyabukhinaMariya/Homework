2b8b85987a22a2af03865d7ab941f360fbfa3bdc
RyabukhinaMariya <1411mashakrasnodar@gmail.com>
2025-09-19 21:02:17 +0300

    Add files via upload

---
+++ HomeWork C/25.09.2025/the remainder of the division.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+    int a, b;
+    int n = 0;
+    int input = scanf("%d" "%d", &a, &b);
+    while (a>=b*(n+1)) {
+        n++;
+    }
+    int res = a-(b*n);
+    if (n == 0) {
+        printf("%d", 0);
+    }
+    else{
+        printf("%d", res);
+    }
+    return 0;
+} 
