36e38739d5944fc7cc045f2362cd9e12917edd2b
RyabukhinaMariya <1411mashakrasnodar@gmail.com>
2025-09-29 11:25:41 +0300

    Create Count_of_the_occurrences.c

---
+++ 30.09.2025/Count_of_the_occurrences.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+int count_occurrences(char *S, char *S1){
+    int count = 0;
+    char *pos = S;
+    while ((pos = strstr(pos, S1)) != NULL){
+        ++count;
+        ++pos;
+    }
+    return count;
+}
+
+int main(void){
+    char S[32000], S1[32000];
+
+    printf("enter the string S: ");
+    fgets(S, sizeof(S), stdin);
+
+    printf("enter the string S1: ");
+    fgets(S1, sizeof(S1), stdin);
+
+    S[strcspn(S, "\n")] = '\0';
+    S1[strcspn(S1, "\n")] = '\0';
+
+    int res = count_occurrences(S, S1);
+
+    printf("the count of occurrences s1 in s is: %d ", res);
+
+    return 0;
+}
