b38fa32bca1c1cc88071bd6a49027248f1c073b8
RyabukhinaMariya <1411mashakrasnodar@gmail.com>
2025-11-20 18:27:13 +0300

    Update the remainder of the division.c

--- HomeWork C/25.09.2025/the remainder of the division.c
+++ HomeWork C/25.09.2025/the remainder of the division.c
@@ -1,19 +1,28 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(void) {
     int a, b;
     int n = 0;
-    scanf("%d" "%d", &a, &b);
-    while (a>=b*(n+1)) {
-        n++;
-    }
-    int res = a-(b*n);
-    if (n == 0) {
-        printf("%d", 0);
-    }
-    else{
-        printf("%d", res);
-    }
-    return 0;
 
-} 
+    scanf("%d %d", &a, &b);
+
+    int a1 = abs(a);
+    int b1 = abs(b);
+    int sign = 1;
+
+    if ((a != a1 && b == b1) || (a == a1 &&  b != b1)) sign = -1;
+
+    if (b == 0){
+        printf("incorrect input\n");
+        return 0;
+    }
+    
+    while (a1 >= b1*(n + 1)) n++;
+
+    n*=sign;
+
+    printf("%d\n", n);
+
+    return 0;
+}
