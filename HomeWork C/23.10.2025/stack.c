21b1f237e5d61666766e2cfe767c5aabc9c440e7
RyabukhinaMariya <1411mashakrasnodar@gmail.com>
2025-11-20 15:12:33 +0300

    Update stack.c

--- HomeWork C/23.10.2025/stack.c
+++ HomeWork C/23.10.2025/stack.c
@@ -1,45 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
-
-typedef struct tag_obg {
-    char data;
-    struct tag_obg* next; //указывает на топ стека
-} OBJ;
-
+#include "stack.h"
 
 OBJ* push(OBJ* top, char data){
-    OBJ* ptr = malloc(sizeof(OBJ)); //создаем указатель на новую выделяемую память
-    ptr -> data = data; //передаем новое значение в стек
-    ptr -> next = top; //некст снова указывает на топ стека
+    OBJ* ptr = malloc(sizeof(OBJ));
+    ptr->data = data;
+    ptr->next = top;
     return ptr;
 }
 
-
 OBJ* pop(OBJ* top){
     if (top == NULL){
-        return top; //если стек пуст, ничего не предпринимаем
+        return top;
     }
-
-    OBJ* ptr_next = top -> next; // есть объект и указатель некст на него, 
+    OBJ* ptr_next = top->next;
     free(top);
-
     return ptr_next;
 }
-
 
 void show(const OBJ* top){
     const OBJ* current = top;
     while(current != NULL){
-        printf("%c\n", current -> data);
-        current = current -> next;
+        printf("%c\n", current->data);
+        current = current->next;
     }
-}
-
-
-int main(void){
-    OBJ* top = NULL;
-
-    return 0;
 }
