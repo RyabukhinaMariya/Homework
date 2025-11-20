7116528e383925b7e34a82db7b2e97ed5ad67578
RyabukhinaMariya <1411mashakrasnodar@gmail.com>
2025-11-20 15:21:40 +0300

    Update Stack+Correct string.c

--- HomeWork C/Stack+Correct string.c
+++ HomeWork C/Stack+Correct string.c
@@ -1,42 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-
-typedef struct tag_obg {
-    char data;
-    struct tag_obg* next; //указывает на топ стека
-} OBJ;
-
-
-OBJ* push(OBJ* top, char data){
-    OBJ* ptr = malloc(sizeof(OBJ)); //создаем указатель на новую выделяемую память
-    ptr -> data = data; //передаем новое значение в стек
-    ptr -> next = top; //некст снова указывает на топ стека
-    return ptr;
-}
-
-
-OBJ* pop(OBJ* top){
-    if (top == NULL){
-        return top; //если стек пуст, ничего не предпринимаем
-    }
-
-    OBJ* ptr_next = top -> next; // есть объект и указатель некст на него, 
-    free(top);
-
-    return ptr_next;
-}
-
-
-void show(const OBJ* top){
-    const OBJ* current = top;
-    while(current != NULL){
-        printf("%c\n", current -> data);
-        current = current -> next;
-    }
-}
-
+#include "stack.h"
 
 int correct(OBJ* top, char* elts, int len){
     for (int i = 0; i < len; ++i){
@@ -45,12 +10,10 @@ int correct(OBJ* top, char* elts, int len){
         }
         else if (elts[i] == ')' || elts[i] == ']' || elts[i] == '}'){
             // проверяем, что стек не пуст и закрывающие скобки подходящего типа с помощью ASKII
-            if (((int) elts[i] - (int) top -> data != 1 && (int) elts[i] - (int) top -> data != 2) || top == NULL ){
-                return 0;
-            }
-            else{
-                top = pop(top);// если все гуд удаляем верхний эл-т стека
-            }
+            if (elts[i] == ')' && top->data != '(') return 0;
+            else if (elts[i] == ']' && top->data != '[') return 0;
+            else if (elts[i] == '}' && top->data != '{') return 0;
+            else top = pop(top);// если все хорошо удаляем верхний эл-т стека
         }
     }
     return top == NULL; // проверяем что не осталось откр скобок
