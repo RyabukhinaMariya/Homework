2b5fa989ab15389a005dc16cd95c3cf990adc9c0
RyabukhinaMariya <1411mashakrasnodar@gmail.com>
2025-11-23 12:12:21 +0300

    Update dialog_array.c

--- HomeWork C/28.10.2025/dialog_array.c
+++ HomeWork C/28.10.2025/dialog_array.c
@@ -6,10 +6,13 @@
 #define DELETE 2
 #define PRINT_ARRAY 3
 
+void show_op(){
+    printf("--Available commands--\n 0 - Exit\n 1 - Add\n 2 - Remove\n 3 - Show array\n");
+}
 
-int* make_new_arr(int* arr, int curr_size){ //закончилось место - создаем массив в 2 раза больше
+float* make_new_arr(float* arr, int curr_size){ //закончилось место - создаем массив в 2 раза больше
     int new_size = curr_size * 2;
-    int* new_arr = (int*)malloc(new_size * sizeof(int));
+    float* new_arr = (float*)malloc(new_size * sizeof(float));
 
     for (int i = 0; i < curr_size; ++i){
         new_arr[i] = arr[i];
@@ -20,18 +23,19 @@ int* make_new_arr(int* arr, int curr_size){ //закончилось место 
     return new_arr;
 }
 
-void print_arr(int* arr, int size_of_arr){
+void print_arr(float* arr, int size_of_arr){
     if (size_of_arr == 0){
         printf("Array is empty");
         return;
     }
+    printf("Your array is: ");
     for (int i = 0; i < size_of_arr; ++i){
-        printf("%d ", arr[i]);
+        printf("%f ", arr[i]);
     }
     printf("\n");
 }
 
-void add(int* array, int count_of_el, int el){ //добавление элемента без нарушения сортированности
+void add(float* array, int count_of_el, float el){ //добавление элемента без нарушения сортированности
     if (count_of_el == 0){
         array[0] = el;
     }
@@ -48,17 +52,20 @@ void add(int* array, int count_of_el, int el){ //добавление элеме
 
 int main(){
     int count_of_el = 0, max_count = 2;
-    int *array = (int*)malloc(max_count * sizeof(int));
-    int op, x;
+    float *array = (float*)malloc(max_count * sizeof(float));
+    int op;
+    float x;
     while (1){
+        show_op();
         if (scanf("%d", &op)  != 1 || op < 0 || op > 3){
             printf("Invalid operation\n");
             while (getchar() != '\n');
         }
         switch (op){
             case ADD:
+            printf("Enter value you want to add: ");
                 if (count_of_el >= max_count) {
-                    int *new_arr = make_new_arr(array, max_count);
+                    float *new_arr = make_new_arr(array, max_count);
                     if (new_arr != NULL){
                         array = new_arr;
                         max_count *= 2;
@@ -68,7 +75,7 @@ int main(){
                         break;
                     }
                 }
-                if (scanf("%d", &x) != 1) {
+                if (scanf("%f", &x) != 1) {
                     printf("Invalid input\n");
                     while (getchar() != '\n');
                     break;
