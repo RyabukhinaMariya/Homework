09d7b92bdefc70fc00b01e67c66f1d7529160d31
RyabukhinaMariya <1411mashakrasnodar@gmail.com>
2025-11-26 20:46:15 +0300

    Create dfs.py

---
+++ HomeWork_Python/28.11.2025/dfs.py
@@ -0,0 +1,24 @@
+class Graph():
+    def __init__(self, graph):
+        if graph: self.graph = graph
+        else: self.graph = []
+
+    def dfs(self, start_node=0, visited=None, inter_arr=None):
+        if visited is None:
+            visited = [0] * len(self.graph)
+        if inter_arr is None:
+            inter_arr = []
+    
+        visited[start_node] = 1
+        inter_arr.append(start_node)
+    
+        for neighbor in range(len(self.graph)):
+            if self.graph[start_node][neighbor] == 1 and not visited[neighbor]:
+                self.dfs(neighbor, visited, inter_arr)
+    
+        return inter_arr
+    
+    def __iter__(self):
+        return iter(self.dfs(0))
+ 
+    pass
