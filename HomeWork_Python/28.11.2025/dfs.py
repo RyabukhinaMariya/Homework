05639d298a1e1ed7052b1a7f2108ffb15848a297
RyabukhinaMariya <1411mashakrasnodar@gmail.com>
2025-12-26 21:28:31 +0300

    Update dfs.py

--- HomeWork_Python/28.11.2025/dfs.py
+++ HomeWork_Python/28.11.2025/dfs.py
@@ -1,26 +1,61 @@
 class Graph():
     def __init__(self, graph):
-        if graph: 
+        if isinstance(graph, list) and all(isinstance(graph[i][j], int) for i in range(len(graph)) for j in range(len(graph[0]))): 
             self.graph = graph
         else: 
-            self.graph = []
+            raise ValueError('invalid garph')
+        
+        self.build_adj_list()
+        
+    def build_adj_list(self):
+        self.adj_list = [[] for _ in range(len(self.graph))]
+        for i in range(len(self.graph)):
+            for j in range(len(self.graph[i])):
+                if self.graph[i][j] == 1:
+                    self.adj_list[i].append(j)
+        return self.adj_list
 
-    def dfs(self, start_node=0, visited=None, inter_arr=None):
+    def dfs(self, start_node=0, visited=None):
         if visited is None:
-            visited = [0] * len(self.graph)
-        if inter_arr is None:
-            inter_arr = []
+            visited = set()
+        
+        res = []
+        stack = [start_node]
+        
+        while stack:
+            vertex = stack.pop()
+            if vertex not in visited:
+                visited.add(vertex)
+                res.append(vertex)
+                for neighbor in reversed(self.adj_list[vertex]):
+                    if neighbor not in visited:
+                        stack.append(neighbor)
+        
+        return res
     
-        visited[start_node] = 1
-        inter_arr.append(start_node)
+    def dfs_all(self):
+        visited = set()
+        res = []
     
-        for neighbor in range(len(self.graph)):
-            if self.graph[start_node][neighbor] == 1 and not visited[neighbor]:
-                self.dfs(neighbor, visited, inter_arr)
+        for vertex in range(len(self)):
+            if vertex not in visited:
+                res.extend(self.dfs(vertex, visited))
     
-        return inter_arr
+        return res
     
     def __iter__(self):
-        return iter(self.dfs(0))
- 
+        return iter(range(len(self)))
+    
+    def __len__(self):
+        return len(self.graph)
+    
+    def __getitem__(self, vertex):
+        return self.adj_list[vertex]
+    
+    def vertices(self):
+         return range(len(self.adj_list))
+    
+    def __repr__(self):
+        return f"graph with {len(self)} vertices"
+
     pass
